7a588cce06ca65a55b6b2625205f977943182e2f
bstanger <beccastanger@gmail.com>
2018-01-29 16:16:31 -0800

    Pass initial tests for VideoList and VideoListEntry

--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,10 +1,21 @@
 var VideoListView = Backbone.View.extend({
 
+  el: '.list',
+
+  initialize: function() {
+    this.collection.on('sync', this.render, this);
+  },
 
   render: function() {
     this.$el.children().detach();
     this.$el.html(this.template());
+    this.collection.forEach(this.renderVideo, this);
     return this;
+  },
+
+  renderVideo: function(video) {
+    var videoView = new VideoListEntryView({ model: video }).render();
+    this.$el.append(videoView.el);
   },
 
   template: templateURL('src/templates/videoList.html')
--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -1,5 +1,16 @@
 var VideoListEntryView = Backbone.View.extend({
 
+  events: {
+    'click .video-list-entry-title': 'handleTitleClick'
+  },
+
+  handleTitleClick: function() {
+    this.model.select();
+  },
+
+  select: function() {
+  
+  },
 
   render: function() {
     this.$el.html(this.template(this.model.attributes));
